7d1e23c98dfdbab0f7ee8fbc23eecdc26471b567
Diego Forganes <diforg@gmail.com>
2025-11-18 15:53:05 -0300

    feat: add and configured Inertia.js

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+const pages = import.meta.glob('./pages/**/*.vue') // importa todas as páginas automaticamente
+
+createInertiaApp({
+  resolve: name => {
+    const page = pages[`./pages/${name}.vue`]
+    if (!page) throw new Error(`Página ${name} não encontrada`)
+    return page()
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
